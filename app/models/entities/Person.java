540e6b46c213664e559c0c5f0cf280bb76082db1
Alexey Antonov <a.antonov@rambler-co.ru>
2016-09-05 01:14:18 +0300

    add comments

--- app/models/entities/Person.java
+++ app/models/entities/Person.java
@@ -2,12 +2,8 @@ package models.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Table;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
 
 @Entity
 @Table(name="persons")
