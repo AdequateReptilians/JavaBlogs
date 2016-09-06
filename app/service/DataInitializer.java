f765422ab1c3fb4cd6f90be7f77d5f0dfe998c57
m.taraskin <m.taraskin@rambler-co.ru>
2016-09-06 18:51:41 +0300

    Managed to run app

--- app/service/DataInitializer.java
+++ app/service/DataInitializer.java
@@ -10,13 +10,14 @@ import java.util.Arrays;
  */
 public class DataInitializer {
     public DataInitializer() {
-        if ((Integer)SecurityRole.session.createCriteria(SecurityRole.class).setProjection(Projections.rowCount()).uniqueResult() == 0) {
-            for (final String roleName : Arrays
-                    .asList(controllers.Application.USER_ROLE)) {
-                final SecurityRole role = new SecurityRole();
-                role.roleName = roleName;
-                SecurityRole.session.save(role);
-            }
-        }
+        // TODO: Fix this code
+//        if ((Integer)SecurityRole.session.createCriteria(SecurityRole.class).setProjection(Projections.rowCount()).uniqueResult() == 0) {
+//            for (final String roleName : Arrays
+//                    .asList(controllers.Application.USER_ROLE)) {
+//                final SecurityRole role = new SecurityRole();
+//                role.roleName = roleName;
+//                SecurityRole.session.save(role);
+//            }
+//        }
     }
 }
